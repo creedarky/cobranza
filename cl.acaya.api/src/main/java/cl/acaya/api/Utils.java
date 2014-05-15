2aefcd3e5a69ac5aba55511b4d91584295b374c7
creedarky <creedarky@gmail.com>
2014-05-15 18:10:18 -0400

    Cambios arquitectura

---
+++ cl.acaya.api/src/main/java/cl/acaya/api/Utils.java
@@ -0,0 +1,13 @@
+package cl.acaya.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maximiliano
+ * Date: 15/05/14
+ * Time: 16:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class Utils {
+
+
+}
