a0f113aa4b158cfbd33d7a5eaecdcca860562cba
Maximiliano Castro <creedarky@gmail.com>
2014-06-12 16:15:01 -0400

    Generación de segunda pantalla

---
+++ cl.acaya.api/src/main/java/cl/acaya/api/vo/Agenda.java
@@ -0,0 +1,56 @@
+package cl.acaya.api.vo;
+
+import java.util.Date;
+
+/**
+ * Created by mcastro on 27-05-14.
+ */
+public class Agenda {
+
+
+    private Date fechaAgendada;
+    private String tipoAgenda;
+    private Long idAgenda;
+    private UsuarioVO usuarioVO;
+
+
+    public ClienteVO getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(ClienteVO cliente) {
+        this.cliente = cliente;
+    }
+
+    public Date getFechaAgendada() {
+        return fechaAgendada;
+    }
+
+    public void setFechaAgendada(Date fechaAgendada) {
+        this.fechaAgendada = fechaAgendada;
+    }
+
+    public String getTipoAgenda() {
+        return tipoAgenda;
+    }
+
+    public void setTipoAgenda(String tipoAgenda) {
+        this.tipoAgenda = tipoAgenda;
+    }
+
+    public Long getIdAgenda() {
+        return idAgenda;
+    }
+
+    public void setIdAgenda(Long idAgenda) {
+        this.idAgenda = idAgenda;
+    }
+
+    public UsuarioVO getUsuarioVO() {
+        return usuarioVO;
+    }
+
+    public void setUsuarioVO(UsuarioVO usuarioVO) {
+        this.usuarioVO = usuarioVO;
+    }
+}
