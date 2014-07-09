1817fb36bcbaa86b83539c55594cc18555f9f16f
Maximiliano Castro <creedarky@gmail.com>
2014-07-09 19:31:16 -0400

    correcciones

--- cl.acaya.api/src/main/java/cl/acaya/api/business/BusinessParameter.java
+++ cl.acaya.api/src/main/java/cl/acaya/api/business/BusinessParameter.java
@@ -8,12 +8,12 @@ public interface BusinessParameter {
     String DB_SCHEMA = "dbo";
     String RUT_CLIENTE = "rutCliente";
     String SOCIEDAD = "sociedad";
-    String IP_SAP = "10.1.24.56";
-    String NAME_SAP = "PRD";
+    String IP_SAP = "1.1.1.1";
+    String NAME_SAP = "QAS";
     String CLIENTE_SAP = "300";
     String SYSTEM_NUMBER = "0";
-    String USER_SAP = "SPKII";
-    String PASSWORD_SAP = "acero2013";
+    String USER_SAP = "sap";
+    String PASSWORD_SAP = "sap";
     String RFC_DATOSCLIENTE = "ZSDFN_DATOS_CLIENTE_11";
     String RFC_CARGOS_SAP = "ZFIFN_SCKFUNCION";
 }
